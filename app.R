4c8f98e9ee2fe760301cb445c85a071a55e00888
kcbeardsley <kcbeardsley@gmail.com>
2026-05-27 13:31:25 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -1,55 +1,82 @@
-#Try to use datatable (DT) function
-#Use this as model: https://yihui.shinyapps.io/DT-click/
+#Use slider range for dates
 #Once functional, adjust the aesthetics: https://rstudio.github.io/DT/
-#Will need to create new crisis-level and actor-level tables with core variables
-    #Create string descriptions to replace the numbers
-#Display key crisis-level variables as text
-#Display key actor-level variables as a datatable
+
 
 library(shiny)
 library(DT)
 library(bslib)
 library(dplyr)
 
+#Change the theme
+#my_theme <- bs_theme(
+#  bootswatch = 'zephyr'
+#)
+
 #Load tables
 full<-read.csv("ICB_Crisis_List_with_Summaries_urls.csv")
 system<-read.csv("ICB_system_core.csv")
 actor<-read.csv("ICB_actor_core.csv")
-
-#Subset to core information without links
 core<-full[,1:5]
+#Create lists of states and pcs and add "ALL"
+countries<-actor$actorname
+countries<-countries[order(unlist(countries))]
+countries<-c("All", unique(countries))
+pcs<-actor$protracted_conflict
+pcs<-pcs[order(unlist(pcs))]
+pcs<-c("All", unique(pcs))
 
 ui <- navbarPage(
+#  theme = my_theme,
   
   title = 'ICB Data Viewer Beta', id = 'x0',
   
-  tabPanel('Crisis List', DT::dataTableOutput('x1')),
+  tabPanel('Crisis List', 
+           selectInput(
+             "state",
+             "Select Crisis Actor",
+             choices = setNames(as.list(countries), countries),
+             selected = "All"
+           ),
+           selectInput(
+             "pc",
+             "Select Protracted Conflict",
+             choices = setNames(as.list(pcs), pcs),
+             selected = "All"
+           ),
+           sliderInput( 
+             "trigyr", "Crisis Trigger Year", 
+             min =  min(actor$trigger_year, na.rm=T), max =  max(actor$trigger_year, na.rm=T), 
+             value = c(min(actor$trigger_year, na.rm=T), max(actor$trigger_year, na.rm=T)),
+             sep = ""
+           ), 
+           DT::dataTableOutput('x1')),
   
   tabPanel('Select Crisis Information', 
            h3(textOutput('name')), 
            uiOutput('summary'),
-           h4('Crisis Actors'), textOutput('actors'),  
-           h4('Trigger Date'), textOutput('trigdate'), 
-           h4('Termination Date'), textOutput('termdate'),  
-           h4('Outcome'), textOutput('outcome'), 
-           h4('Threat Gravity'), textOutput('gravity'), 
-           h4('Violence'), textOutput('violence'), 
+           DT::dataTableOutput('actortable'),
+#          h4('Crisis Actors'), textOutput('actors'),  
+#          h4('Trigger Date'), textOutput('trigdate'), 
+#          h4('Termination Date'), textOutput('termdate'),  
+           h4('Crisis Outcome'), textOutput('outcome'), 
+           h4('Crisis Threat Gravity'), textOutput('gravity'), 
+#          h4('Violence'), textOutput('violence'), 
            h4('Protracted Conflict'), textOutput('pc'), 
-           h4('Mediation'), textOutput('mediation')),
+           h4('Crisis Mediation'), textOutput('mediation')),
   
 # tabPanel('Select Crisis Information', DT::dataTableOutput('systemtable')),
 
-  tabPanel('Select Actor Information', DT::dataTableOutput('actortable'))
-)
+#  tabPanel('Select Actor Information', DT::dataTableOutput('actortable'))
 
+)
 # Define server logic ----
 
 server <- shinyServer(function(input, output, session) {
-  
+
   # add CSS style 'cursor: pointer' to the 1st column (i.e. crisis name)
   output$x1 = DT::renderDataTable({
     datatable(
-      core, selection = 'none', class = 'cell-border strip hover',
+      core[core$Crisis.Name %in% actor$crisname[(actor$actorname==input$state | actor$all==input$state) & (actor$protracted_conflict==input$pc | actor$all==input$pc) & actor$trigger_year>=min(input$trigyr) & actor$trigger_year<=max(input$trigyr)],], selection = 'none', class = 'cell-border strip hover',
       colnames = c('Name', 'Number', 'Actors', 'Trigger Date', 'Termination Date'),
       options = list(pageLength = 600), rownames = FALSE,
       caption='Click on the crisis name for more information'
@@ -61,17 +88,6 @@ server <- shinyServer(function(input, output, session) {
     # do nothing if not clicked yet, or the clicked cell is not in the 1st column
     if (is.null(info$value) || info$col != 0) return()
     updateTabsetPanel(session, 'x0', selected = 'Select Crisis Information')
-#    updateTextInput(session, 'name', value = info$value)
-#    updateTextInput(session, 'crisno', value = full$Crisis.Num.[full$Crisis.Name==info$value])
-#    updateTextInput(session, 'actors', value = full$Actors[full$Crisis.Name==info$value])
-#    updateTextInput(session, 'trigger', value = system$trigger[system$crisname==info$value])
-#    updateTextInput(session, 'trigdate', value = system$trigger_date[system$crisname==info$value])
-#    updateTextInput(session, 'termdate', value = system$termination_date[system$crisname==info$value])
-#    updateTextInput(session, 'gravity', value = system$gravity[system$crisname==info$value])
-#    updateTextInput(session, 'violence', value = system$violence[system$crisname==info$value])
-#    updateTextInput(session, 'outcome', value = system$outcome[system$crisname==info$value])
-#    updateTextInput(session, 'pc', value = system$protracted_conflict[system$crisname==info$value])
-#    updateTextInput(session, 'mediation', value = system$mediation[system$crisname==info$value])
     output$name<-renderText({info$value})
     output$actors<-renderText({full$Actors[full$Crisis.Name==info$value]})
     output$trigdate<-renderText({system$trigger_date[system$crisname==info$value]})
